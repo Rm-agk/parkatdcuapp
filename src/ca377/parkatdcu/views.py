ae6fc180831afb1f9a769234a50bcf7869f0653c
Martins Richmond <79218307+Rm-agk@users.noreply.github.com>
2022-09-28 09:40:45 +0100

    Completed A1

--- src/ca377/parkatdcu/views.py
+++ src/ca377/parkatdcu/views.py
@@ -4,17 +4,20 @@ from .models import Campus,Carpark
 
 # Create your views here.
 
-def index(request):
+'''def index(request):
     campus_data = Campus.objects.all()
     carparks_in_campus = Carpark.objects.all()
     for Carpark in campus_data:
         carparks = Carpark.objects.filter(campus_id = campus.campus_id)
         carparks_in_campus[campus.name] = carparks
-    print(carparks_in_campus)
+    print(carparks_in_campus)'''
 
-
+def index(request):
+    campus_name = Campus.objects.all()
+    carpark_name = Carpark.objects.all()
+    no_parking = Campus.objects.filter(carpark=None)
     context = {
-        'carparks_in_campus': carparks_in_campus
+        'campus_name': campus_name, 'carpark_name': carpark_name, 'no_parking': no_parking
     }
     # retrieve carpark information from the database
     # add it to the context dictionary
